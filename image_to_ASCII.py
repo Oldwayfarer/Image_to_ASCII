1c682d2154d4fcabbcd1e520f2e391803c81d9ea
oldwayfarer <oldwaybeing@yandex.ru>
2018-04-07 15:14:09 +0300

    curses integration

--- image_to_ASCII.py
+++ image_to_ASCII.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import curses
 import argparse
 import json
 import random
@@ -9,8 +10,8 @@ default_ascii_dict = {
         '0':[' '],
         '1':['.'],
         '2':[',', '-'],
-        '3':[':', ';'],
-        '4':['*', '+', '='],
+        '3':['*', '+', '='],
+        '4':[':', ';'],
         '5':['x', 'o', 'n'],
         '6':['l', 'k', 'j'],
         '7':['f', 't', 'h'],
@@ -23,6 +24,49 @@ default_ascii_dict = {
         '14':['B'],
         '15':['M']
 }
+def end_stdscr(stdscr):
+    curses.nocbreak()
+    curses.echo()
+    stdscr.keypad(True)
+    curses.endwin()
+
+def Image_displaying(stdscr, lines):
+    for i in range(0, curses.COLS):
+        stdscr.addstr(curses.LINES - 2, i, " ", curses.A_REVERSE)
+    stdscr.refresh()
+    stdscr.addstr(curses.LINES-1, 0, "[arrows key] - navigation [q] - exit")
+    if len(lines[0]) > curses.COLS-1:
+        visible_cols = curses.COLS
+    else:
+        visible_cols = len(lines[0])
+    if len(lines) > curses.LINES - 3:
+        visible_lines = curses.LINES - 2
+    else:
+        visible_lines = len(lines)
+    for i in range(0,visible_lines):
+        stdscr.addstr(i, 0, lines[i][:visible_cols])
+    stdscr.refresh()
+    current_line = 0
+    current_col = 0
+    while True:
+        key = stdscr.getch()
+        if key == ord('q'):
+            break
+        elif key == 259:
+            if current_line > 0 :
+                current_line -= 1
+        elif key == 258:
+            if current_line < len(lines)-curses.LINES:
+                current_line += 1
+        elif key == 260: 
+            if current_col > 0:
+                current_col -= 1
+        elif key == 261:
+            if current_col < len(lines[0])-curses.COLS:
+                current_col += 1
+        for i in range(0,visible_lines):
+            stdscr.addstr(i, 0, lines[i+current_line][current_col:visible_cols+current_col])
+        stdscr.refresh()
 
 def get_average(pixel):
     """Return the average value of three pixel parameters."""
@@ -85,72 +129,111 @@ def main():
                         help = 'New json vocabulary file. Format: {"Range":["List of symbols"]}. "default" to set default values')
     args = parser.parse_args()
 
+    stdscr = curses.initscr()
+    stdscr.keypad(True)
+    curses.noecho()
+    curses.curs_set(0)
+
     try:
-        with Image.open(args.Image_name) as image:
-            options = json_load()
-            size = 100
-            adjustment = 1.5
-            dictionary = default_ascii_dict
-            if options:
-                 size = int(options[0])
-                 adjustment = float(options[1])
-                 dictionary = options[2]
-            if args.w:
-                if args.w<0:
-                    print('Width can not be negative: -w ', args.w)
-                    exit(-1)
-                size = args.w
-            if args.a:
-                if args.a < 0:
-                    print('Adjustment can not be negative: -a ', args.a)
-                adjustment = args.a
-            if args.d:
-                if args.d.upper() == 'DEFAULT':
-                    dictionary = default_ascii_dict
-                else:
-                    try:
-                        json_dict = open(args.d)
-                        new_dict = json.load(json_dict)
-                        if save_dict_checker(new_dict):
-                            dictionary = new_dict
-                        else:
-                            if not dictionary:
-                                dictionary = default_ascii_dict
-                    except FileNotFoundError:
-                        print('Can not find file "{}"'.format(args.d))
-                        exit(-1)
-                    except json.decoder.JSONDecodeError:
-                        print('Wrong format of save file')
-                        exit(-1)
-            if args.s:
-                json_dump(size, adjustment, dictionary)
-
-            width,height = image.size
-            if adjustment == 1:
-                adjustment += 0.001
-            width = int(width * adjustment)
-            image = image.resize((width, height), Image.ANTIALIAS)
-            pixels = image.load()
-            i_shift = 0
-            j_shift = 0
-            average = 0
-            shift = width // size
-            while i_shift+shift < height:
-                while j_shift + shift < width:
-                    for i in range(i_shift, shift + i_shift):
-                        for j in range(j_shift, shift + j_shift):
-                            average += get_average(pixels[j, i])
-                    print(get_ascii(average // (shift**2), dictionary), end = '')
-                    average = 0
-                    j_shift += shift
-                j_shift = 0
-                i_shift += shift
-                print('')
+        image = Image.open(args.Image_name)
     except FileNotFoundError:
+        end_stdscr(stdscr)
         print('file "{}" Doesn\'t exist'.format(args.Image_name))
+        exit(-1)
     except OSError:
+        end_stdscr(stdscr)
         print('can not identify image: "{}"'.format(args.Image_name))
-
+        exit(-1)
+    options = json_load()
+    size = 100
+    adjustment = 1.5
+    dictionary = default_ascii_dict
+    if options:
+        size = int(options[0])
+        adjustment = float(options[1])
+        dictionary = options[2]
+    if args.w:
+        if args.w < 0:
+            end_stdscr(stdscr)
+            print('Width can not be negative: -w ', args.w)
+            exit(-1)
+        size = args.w
+    if args.a:
+        if args.a < 0:
+            end_stdscr(stdscr)
+            print('Adjustment can not be negative: -a ', args.a)
+            exit(-1)
+        adjustment = args.a
+    if args.d:
+        if args.d.upper() == 'DEFAULT':
+            dictionary = default_ascii_dict
+        else:
+            try:
+                json_dict = open(args.d)
+                new_dict = json.load(json_dict)
+                json_dict.close()
+                if save_dict_checker(new_dict):
+                    dictionary = new_dict
+                else:
+                    if not dictionary:
+                        dictionary = default_ascii_dict
+            except FileNotFoundError:
+                end_stdscr(stdscr)
+                print('Can not find file "{}"'.format(args.d))
+                exit(-1)
+            except json.decoder.JSONDecodeError:
+                end_stdscr(stdscr)
+                print('Wrong format of save file')
+                exit(-1)
+    if args.s:
+        json_dump(size, adjustment, dictionary)
+    stdscr.addstr(1,1,'Converting image to ASCII...', curses.A_BOLD)
+    stdscr.addstr(2,0,"0 %")
+    stdscr.addstr(2,4,"[          ]",curses.A_BOLD)
+    width,height = image.size
+    if adjustment == 1:
+        adjustment += 0.001
+    width = int(width * adjustment)
+    image = image.resize((width, height), Image.ANTIALIAS)
+    pixels = image.load()
+    image.close()
+    line = ''
+    lines = []
+    i_shift = 0
+    j_shift = 0
+    average = 0
+    shift = width // size
+    area = (height//shift) * (width//shift)
+    current_area = 0
+    current_percent = 0
+    spaces = 0
+    flag =0
+    while i_shift + shift < height:
+        while j_shift + shift < width:
+            for i in range(i_shift, shift + i_shift):
+                for j in range(j_shift, shift + j_shift):
+                    average += get_average(pixels[j, i])
+            line += get_ascii(average // (shift**2), dictionary)
+            current_area += 1
+            percent = (current_area*100)//area
+            if current_percent != percent:
+                current_percent = percent
+                stdscr.addstr(2, 0, str(percent))
+                if percent%10 == 0 and percent//10 != flag:
+                    flag = percent//10
+                    spaces += 1
+                    stdscr.addstr(2,spaces+4, ' ', curses.A_REVERSE)
+                stdscr.refresh()
+            average = 0
+            j_shift += shift
+        j_shift = 0
+        i_shift += shift
+        lines.append(line)
+        line = ''
+    stdscr.clear()
+    stdscr.refresh()
+    Image_displaying(stdscr, lines)
+    end_stdscr(stdscr)
+    
 if __name__ == "__main__":
     main()
-
